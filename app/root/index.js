8ab6f843e628c585212b95ada4604a1908e54ed7
Apollo Tang <tang.apollo@gmail.com>
2017-05-24 21:31:06 -0400

    added decorator, added connectedNavigator

--- app/root/index.js
+++ app/root/index.js
@@ -1,31 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import {
-  View,
-  Text
-} from 'react-native';
+  Provider,
+  connect
+} from 'react-redux';
 
-import {Provider} from 'react-redux';
 import store from './store';
 
 import Screen_home from '../modules/screen-home';
 
 console.log(store.getState())
 
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <Provider store={store} >
+//         <Screen_home />
+//       </Provider>
+//     );
+//   }
+// };
+
+const mapStateToProps = state => ({
+  nav: state.nav,
+});
+
+@connect(mapStateToProps)
+class ConnectedNavigator extends React.Component {
+  render() {
+    console.log('xxxxx: ', this.props)
+    return this.props.children;
+  }
+};
+// ConnectedNavigator.propTypes = {
+//   dispatch: PropTypes.func.isRequired,
+//   nav: PropTypes.object.isRequired,
+// };
+
 class App extends React.Component {
   render() {
     return (
       <Provider store={store} >
-        <Screen_home />
+        <ConnectedNavigator>
+          <Screen_home />
+        </ConnectedNavigator>
       </Provider>
     );
   }
 };
 
-
 export default App;
 
 
-function rootReducer(state = {'someState': 'sssomeState'}, action) {
-  return state
-}
