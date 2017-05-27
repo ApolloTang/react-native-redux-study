8d9d6101c414bfd3839df56ae5949d691304196e
Apollo Tang <tang.apollo@gmail.com>
2017-05-27 11:44:00 -0400

    make props in RootNavigator available to ConnectedNavigator; add explain in comment

--- app/root/connected-navigator.js
+++ app/root/connected-navigator.js
@@ -16,15 +16,31 @@ class ConnectedNavigator extends React.Component {
     dispatch: PropTypes.func.isRequired,
     nav: PropTypes.object.isRequired,
   }
+  componentDidMount(prevProps, prevState) {
+    console.log('componentDidMount: props_rootNavigator: ', this.props_rootNavigator);
+  }
   render() {
+    console.log("ConnectedNavigator's property: ", this.props);
     const { dispatch , nav } = this.props;
-    console.log("ConnectedNavigator's property: ", this.props)
     return (
     <RootNavigator
-      navigation={addNavigationHelpers({ dispatch, state: nav })}
+      ref={props_rootNavigator=>{
+        // Make properties of rootNavigator available to this component (ie. ConnectedNavigator).
+        // We do this because we might want to have access to these properties in this
+        // compoenent.
+        this.props_rootNavigator = props_rootNavigator
+      }}
+      navigation={
+        // When navigation propperty is specifed React
+        // Navigation state is now store in reducer, React Navigation
+        // will no longer keep track of navigation state (nav)
+        addNavigationHelpers({ dispatch, state: nav })
+      }
       />
     )
-
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate: props_rootNavigator: ', this.props_rootNavigator);
   }
 };
 
