f7e9e68a27fa81586e5642cb3dbfc57965969936
Apollo Tang <tang.apollo@gmail.com>
2017-05-27 00:26:38 -0400

    navigation with dispatch in the present of reducer Success!!!

--- app/modules/screen-home.js
+++ app/modules/screen-home.js
@@ -2,8 +2,15 @@ import React from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity
 } from 'react-native';
+
+
+import {NavigationActions} from 'react-navigation';
+
+const Action_navigateToRoute = (routeName) => NavigationActions.navigate({ routeName });
+const mapStateToProps = state => ({ 'someState': 'someState' });
 
 class Screen extends React.Component {
   render() {
@@ -12,6 +19,17 @@ class Screen extends React.Component {
         <Text style={styles.text}>
           Home Screen
         </Text>
+        <TouchableOpacity
+          onPress={ ()=>{
+            console.log('TouchableOpacity pressed', Action_navigateToRoute('ScreenA') );
+            this.props.navigation.dispatch(
+              Action_navigateToRoute('ScreenA')
+            )
+          } }>
+          <View>
+            <Text>Navigate to Screen A</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
