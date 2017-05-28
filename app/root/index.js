48d0ce85b4a213068559fb402508cd68e7de9458
Apollo Tang <tang.apollo@gmail.com>
2017-05-27 23:12:31 -0400

    Test drive Reactotron, connected sucessfully to the cli but not the app

--- app/root/index.js
+++ app/root/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Reactotron from 'reactotron-react-native'
 
 import {
   Provider,
@@ -17,6 +18,25 @@ class App extends React.Component {
     // see https://github.com/reactjs/react-redux/issues/359
   }
   render() {
+    // Test drive Reactotron
+    //
+    Reactotron.log('hello rendering world');
+    Reactotron.log({ numbers: [1, 2, 3], boolean: false, nested: { here: 'we go' } })
+    Reactotron.warn('*glares*')
+    Reactotron.error('Now you\'ve done it.')
+    Reactotron.display({
+      name: 'KNOCK KNOCK',
+      preview: 'Who\'s there?',
+      value: 'Orange.'
+    })
+
+    Reactotron.display({
+      name: 'ORANGE',
+      preview: 'Who?',
+      value: 'Orange you glad you don\'t know me in real life?',
+      important: true
+    })
+
     return (
       <Provider store={this.state.store} >
         <ConnectedNavigator/>
--- index.ios.js
+++ index.ios.js
@@ -1,3 +1,5 @@
+import './ReactotronConfig';
+
 import React from 'react';
 
 import {
